a529a545cd03b7b2b2dfe996164d1c4cbf795437
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-02-16 12:30:51 +0700

    Update chessboard

--- model/chessboard.js
+++ model/chessboard.js
@@ -1,6 +1,10 @@
 let chessBoard_image = new Image();
 chessBoard_image.src = "assets/images/chessboard/chessboard.png";
 
+let chessBoard_bg_image = new Image();
+chessBoard_bg_image.src = "assets/images/chessboard/chessboard_bg.png";
+
+
 class ChessBoard {
     constructor(game) {
         this.game = game;
@@ -11,6 +15,8 @@ class ChessBoard {
     }
 
     draw() {
+        let size_2 = this.size / 2;
         this.game.context.drawImage(chessBoard_image, this.x, this.y, this.size * 4, this.size * 4);
+        this.game.context.drawImage(chessBoard_bg_image, this.x - size_2 / 2, this.y - size_2 / 2, this.size * 4 + size_2, this.size * 4 + size_2);
     }
 }
