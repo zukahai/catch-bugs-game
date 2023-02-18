7b9d1913cfcf4bd89c655c2f8e1ae2a114aa5ee1
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-02-19 00:09:53 +0700

    add alogrithm

--- utils/algorithm.js
+++ utils/algorithm.js
@@ -1,5 +1,108 @@
 class Algorithm {
     constructor() {
         console.log("Algorithm");
+        this.result = [];
+        this.N_result = 0;
+        this.matrix = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ];
+        this.block = [
+            [
+                [2, 0],
+                [1, 2]
+            ],
+            [
+                [1, 0],
+                [2, 1]
+            ],
+            [
+                [2, 0],
+                [1, 1]
+            ],
+            [
+                [2, 0],
+                [1, 1]
+            ],
+            [
+                [1, 2]
+            ],
+            [
+                [1, 1]
+            ]
+        ];
+        this.solve();
     }
+
+    copyMatrix(matrix) {
+        let temp = [];
+        for (let i = 0; i < matrix.length; i++) {
+            let temp2 = [];
+            for (let j = 0; j < matrix[0].length; j++)
+                temp2[j] = matrix[i][j];
+            temp[i] = temp2;
+        }
+        return temp;
+    }
+
+    solve() {
+
+        this.N = 4;
+        let matrix = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ];
+        this.dfs(1, matrix, this.copyMatrix(matrix));
+        console.log(this.result);
+    }
+
+    isLocation(matrix, block, indexI, indexJ) {
+        // console.log(matrix);
+        if (indexI + block.length - 1 >= matrix.length)
+            return false;
+        if (indexJ + block[0].length - 1 >= matrix[0].length)
+            return false;
+        for (let i = 0; i < block.length; i++)
+            for (let j = 0; j < block[0].length; j++)
+                if (matrix[indexI + i][indexJ + j] * block[i][j] != 0)
+                    return false;
+        return true;
+    }
+
+    dfs(index, matrix, R) {
+        let result_temp = this.copyMatrix(matrix);
+        let R_temp = this.copyMatrix(R);
+        let block_current = this.block[index - 1];
+        for (let rotate = 1; rotate <= 8; rotate++) {
+            result_temp = result_temp = this.copyMatrix(matrix);
+            R_temp = this.copyMatrix(R);
+            let m = block_current.length;
+            let n = block_current[0].length;
+            for (let i = 0; i < this.N; i++)
+                for (let j = 0; j < this.N; j++) {
+                    if (this.isLocation(result_temp, block_current, i, j)) {
+                        for (let i2 = 0; i2 < m; i2++)
+                            for (let j2 = 0; j2 < n; j2++) {
+                                result_temp[i + i2][j + j2] += block_current[i2][j2];
+                                if (block_current[i2][j2] != 0)
+                                    R_temp[i + i2][j + j2] = index;
+                            }
+                        if (rotate == 4)
+                            block_current = Matrix.symmetryBlock(block_current);
+                        if (index == 6) {
+                            this.result[this.N_result++] = { data: result_temp, result: R_temp };
+                        } else
+                            this.dfs(index + 1, result_temp, R_temp);
+                    }
+                }
+            block_current = Matrix.rotateBlockNext(block_current);
+
+        }
+    }
+
+
 }
